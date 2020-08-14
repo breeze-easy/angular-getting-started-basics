8476ecb4a2937f4cce7a13c627aed6a47445561f
andrew <androdev.cft6@gmail.com>
2020-08-13 20:18:56 -0400

    fist commit

---
+++ src/app/products.ts
@@ -0,0 +1,39 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Pixel 3 XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Pixel 4',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'iPhone 11',
+    price: 1299,
+    description: 'Apple iPhone 11'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
